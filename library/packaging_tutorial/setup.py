889852eca4811dcaf14ec06bb52d02ed787a5931
BinhPhanVan <phanvanbinh192837@gmail.com>
2022-09-20 21:24:28 +0700

    init nexus repository

---
+++ library/packaging_tutorial/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="python-bchtq",
+    version="0.0.1",
+    author="BinhPhanVan",
+    author_email="phanvanbinh192837@gmail.com",
+    description="Sum function",
+    long_description="Full description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
